29cdca1d2599f095137714c5f7f1b69189d1873c
Mike Sperber <sperber@deinprogramm.de>
2021-08-27 15:20:06 +0200

    Exercise

--- hearts/src/GameEvent.hs
+++ hearts/src/GameEvent.hs
@@ -3,15 +3,5 @@ module GameEvent where
 import Cards
 
 data GameEvent
-  = HandDealt Player Hand
-  | PlayerTurnChanged Player
-  | LegalCardPlayed Player Card
-  | TrickTaken Player Trick
-  | IllegalCardPlayed Player Card
-  | GameEnded Player
-  deriving (Eq, Show)
 
 data GameCommand
-  = DealHands PlayerHands
-  | PlayCard Player Card
-  deriving (Eq, Show)
